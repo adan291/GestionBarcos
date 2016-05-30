02f75e53dcb59370bc18eadab2819dcd585edb84
adan291 <adanx05@gmail.com>
2016-05-30 12:55:44 +0200

    Test001

--- PuertoTest.java
+++ PuertoTest.java
@@ -39,4 +39,27 @@ public class PuertoTest
     public void tearDown()
     {
     }
+
+    @Test
+    public void test001()
+    {
+        Puerto puerto1 = new Puerto();
+        Velero velero1 = new Velero(2, "AAA", 12, 1990);
+        EmbarcacionDeportiva embarcac1 = new EmbarcacionDeportiva(1200, "BBB", 40, 2014);
+        Yate yate1 = new Yate(8, 4000, "CCC", 23, 2001);
+        Velero velero2 = new Velero(3, "DDD", 5, 2016);
+        Cliente cliente1 = new Cliente("enrique", "5555k");
+        Cliente cliente2 = new Cliente("juan", "5555D");
+        Cliente cliente3 = new Cliente("cristiano", "5555F");
+        Cliente cliente4 = new Cliente("gerald", "5555G");
+        assertEquals(0, puerto1.addAlquiler(3, cliente1, velero1));
+        assertEquals(1, puerto1.addAlquiler(4, cliente2, embarcac1));
+        assertEquals(2, puerto1.addAlquiler(5, cliente3, yate1));
+        assertEquals(361600, puerto1.liquidarAlquiler(1), 0.1);
+        assertEquals(-1, puerto1.liquidarAlquiler(27), 0.1);
+        assertEquals(-1, puerto1.liquidarAlquiler(1), 0.1);
+        assertEquals(1, puerto1.addAlquiler(4, cliente4, velero2));
+        puerto1.verEstadoAmarres();
+    }
 }
+
